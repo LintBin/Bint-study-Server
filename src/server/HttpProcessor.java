780b363d4e12a497b99ef0ddaa70037985e5c231
LintBin <312763889@qq.com>
2016-12-08 00:24:08 +0800

    add HttpConnector and HttpProcessor

---
+++ src/server/HttpProcessor.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.net.Socket;
+
+import server.http.HttpResponse;
+
+
+public class HttpProcessor {
+	
+	
+	private HttpResponse response;
+
+	public HttpProcessor(HttpConnector connection){
+		
+		
+	}
+	
+	
+	public void process(Socket socket){
+		
+	}
+	
+}
---
+++ src/server/http/HttpRequest.java
@@ -0,0 +1,15 @@
+package server.http;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class HttpRequest {
+	
+	protected HashMap<String,String> header ;
+	protected ArrayList<Object> cookies;
+	//protected ParameterMap parameters;
+	
+	
+	
+	
+}
